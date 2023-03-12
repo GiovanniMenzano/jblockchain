eed45210e07d8fe6419b8f8c091835b9fc9fde2a
Giovanni Menzano <giovanni.menzano@gmail.com>
2023-03-12 04:00:22 +0100

    initial commit.

---
+++ src/test/java/com/giovannimenzano/jblockchain/JblockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovannimenzano.jblockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JblockchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
